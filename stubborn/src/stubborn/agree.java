744e4385b586eaac788d423712ec9b28fb72b6d6
cmyipaa <cmyipaa@connect.ust.hk>
2015-03-01 23:04:58 +0800

    testing on git

---
+++ stubborn/src/stubborn/agree.java
@@ -0,0 +1,10 @@
+package stubborn;
+
+public class agree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("you are very stubborn...");
+	}
+
+}
