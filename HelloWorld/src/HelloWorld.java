f54950c7bc5adb06d4ad7820f46474d564ca8153
cmyipaa <cmyipaa@connect.ust.hk>
2015-03-01 22:23:00 +0800

    testing

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub111111
-		System.out.println("Hello World"); ///1111
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 	}
 
 }
