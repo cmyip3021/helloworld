45ae03b1ee97429c1bdf29b4fc228c091c3b66a2
cmyipaa <cmyipaa@connect.ust.hk>
2015-03-01 23:43:00 +0800

    123123

--- stubborn/src/stubborn/agree.java
+++ stubborn/src/stubborn/agree.java
@@ -4,7 +4,7 @@ public class agree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("you are very stubborn...");
+		System.out.println("you are very aaaaaaaaaa...");
 	}
 
 }
